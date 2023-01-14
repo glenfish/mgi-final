0e0f954a7241a6fece4f3f07f458fc7c1f3987c7
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2023-01-14 13:23:24 +0700

    defer js and wistia video

---
+++ public/js/wistiaload.js
@@ -0,0 +1,6 @@
+
+    var videoContent = document.getElementById('wistia');
+    console.log(videoContent);
+    var videoDummyContent = document.getElementById('wistia-load');
+    videoDummyContent.classList.add('hide');
+    videoContent.classList.remove('hide');
